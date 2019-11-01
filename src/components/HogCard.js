5eabdbc7ae865e58beb81873476a5f3e2e70bf06
BennyLouie <bennylouie1412@gmail.com>
2019-11-01 11:28:51 -0400

    removed redundant method

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -4,16 +4,6 @@ import CardFront from './CardFront'
 import CardBack from './CardBack'
 
 export default class HogCard extends Component {
-
-    findPic = (name) => {
-        let newName = name.toLowerCase()
-        if (newName.includes(" ")){
-            let picName= newName.split(" ").join("_")
-            return picName
-        }
-        return newName
-    }
-
     render(){
         return( 
             <div className="ui card hog-card" style={{visibility:'visible'}}>
